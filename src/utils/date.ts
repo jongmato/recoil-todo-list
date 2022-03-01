791996e5e88a1f8ea03ed61657caeb78f61756c5
jonghyun <jonghyeon2046@gmail.com>
2022-03-01 17:42:07 +0900

    [UPDATE]주석 추가

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -2,6 +2,7 @@ export const pad = (time: number) => {
 	return `0${time}`.slice(-2);
 };
 
+// yyyy-mm-dd 형태로 변경 해주는 함수 
 export const getSimpleDateFormat = (d: Date, separator: string = "-") => {
 	return [d.getFullYear(), pad(d.getMonth() + 1), pad(d.getDate())].join(separator);
 };
